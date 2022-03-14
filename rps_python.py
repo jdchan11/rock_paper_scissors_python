fd62dc5724201809405ffe559b425ddc0cc9ad8f
jdchan11 <97546230+jdchan11@users.noreply.github.com>
2022-03-14 11:32:14 -0400

    Add files via upload
    Second commit to fix changes

--- rps_python.py
+++ rps_python.py
@@ -1,12 +1,17 @@
-from random import choice
+import random
 
 options = ['Rock', 'Paper', 'Scissors']
 
 def rps_random():
     
     while True:    
-        computer_output = choice(options)
-        computer_output = computer_output.lower()
+        computer_output = random.randint(1,4)
+        if computer_output == 1:
+            computer_output = 'rock'
+        elif computer_output == 2:
+            computer_output = 'paper'
+        else:
+            computer_output = 'scissors'
         user_input = input("\nWhat is your choice: rock, paper, or scissors? ")
         user_input = user_input.lower()
         if computer_output == user_input:
@@ -24,6 +29,7 @@ def rps_random():
         elif computer_output == 'scissors' and user_input == 'paper':
             print("You Lose")
         elif user_input == "i quit":
+            print("\nThank you for playing!")
             break
  
 
